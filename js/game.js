8e7c19d63fc0101a45b05ad3a71796d0daaf9a2a
francism98 <francismanubag@gmail.com>
2021-02-18 17:41:45 +0800

    The first commit

---
+++ js/game.js
@@ -0,0 +1,55 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+  {
+    question: "Inside which HTML element do we put the Javascript?",
+    choice1: "<script>",
+    choice2: "<javacsript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+  },
+  {
+    question: "What is the correct syntax for referring to an external script called xxx.js?",
+    choice1: "<script href='xxx.js>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  }
+]
+
+// CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...]
+}
+
+let sum2 = (a, b) => a + b;
+
+let isPositive = (number) => number >= 0;
+
+let randomNumber = () => Math.random
+
+document.addEventListener('click', () => console.log('Click'))
+
+console.log(sum2(2,1));
